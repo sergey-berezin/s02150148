4c32148103f3f4b45975eaf4c092ab515e42d1b6
ShishkaDreamer <9971180@inbox.ru>
2020-12-07 00:19:39 +0300

    Tak 1, Test 1
    Первая попытка сдачи лабораторной 1, вариант 2а

---
+++ Lefler_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using OnnxClassifier;
+
+namespace Task_1
+{
+
+    class Program
+    {
+        static void RecognitionCompletedHandler(ResultClassification result)
+        {
+            Console.WriteLine(result);
+        }
+
+        static void Main(string[] args)
+        {
+            string directoryPath;
+
+            if (args.Length == 0)
+                directoryPath = "../../../../../Samples";
+            else
+                directoryPath = args[0];
+
+
+            OnnxClassifier.OnnxClassifier onnxModel = new OnnxClassifier.OnnxClassifier();
+
+            ThreadClassification task_1 = new ThreadClassification(directoryPath, onnxModel, RecognitionCompletedHandler);
+
+            Console.WriteLine("Press key to stop me...");
+
+            task_1.Run();
+
+            Console.ReadKey(true);
+            task_1.Stopper();
+
+
+        }
+    }
+}
