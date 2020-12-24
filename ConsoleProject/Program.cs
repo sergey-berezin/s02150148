92135a327c7b186d7933855ec7fc4760ee4a18d3
ShishkaDreamer <9971180@inbox.ru>
2020-12-24 21:52:48 +0300

    Lefler Task 2-3
    Lefler Iosuf 401

---
+++ ConsoleProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using PredictorLibrary;
+
+namespace ConsoleProject
+{
+    class Program
+    {
+        public static void Output(Result result)
+        {
+            Console.WriteLine(result);
+        }
+
+        static void Main(string[] args)
+        {
+            var a = new Predictor("../../../../Samples/", Output);
+            Console.WriteLine(a.ToString());
+            Console.CancelKeyPress += (sender, eArgs) => {
+                a.Stop();
+                eArgs.Cancel = true;
+            };
+            a.ProcessDirectory();
+        }
+    }
+}
